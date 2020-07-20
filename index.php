d97441a0ebc2a8dcde84cb26641c8af4e76e290d
Danny <ugarte1chirapa1danny@gmail.com>
2020-07-19 23:38:02 -0500

    Formulario con nombre y apellido y git

--- index.php
+++ index.php
@@ -9,14 +9,16 @@ echo <<<_END
         
         <form action="index.php"  method="post">
         Nombre  <input type="text" name="nombre">
+        Nombre  <input type="text" name="apellido">
         <input type="submit" value="INGRESAR">
         </form>
 _END;
 
 
-if(isset($_POST["nombre"] )){
+if(isset($_POST["nombre"] && isset($_POST["apellido"]) )){
 
-    echo "Tu nombre es ".$_POST["nombre"];
+    echo "Tu nombre es ".$_POST["nombre"]."<br>";
+    echo "Tu apellido es ".$_POST["apellido"];
 }
 
 
